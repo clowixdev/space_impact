27538d9303573b0a064988f6b06ca19505e8f560
Nikita <clowixdev@gmail.com>
2024-10-26 02:46:38 +0300

    added build script for Linux

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+x86_64-w64-mingw32-gcc "${workspaceRoot}src/main.c" "-g" "--std=c99" \
+"${workspaceRoot}src/keyboard.c" \
+"${workspaceRoot}src/update.c" \
+"${workspaceRoot}src/draw.c" \
+"${workspaceRoot}src/init.c" \
+"-I${workspaceRoot}src/include" \
+"-I${workspaceRoot}../dependencies/STB_IMAGE/include" \
+"-I${workspaceRoot}../dependencies/GLFW/include" \
+"-I${workspaceRoot}../dependencies/FREEGLUT/include" \
+"-I${workspaceRoot}../dependencies/GLEW/include" \
+"-lwinmm" \
+"-lopengl32" \
+"-lglu32" \
+"-L${workspaceRoot}../dependencies/GLEW/bin/Release/x64" \
+"-lglew32" \
+"-L${workspaceRoot}../dependencies/FREEGLUT/bin/x64" \
+"-lfreeglut" \
+"-L${workspaceRoot}../dependencies/GLFW/lib-mingw-w64" \
+"-lglfw3dll" \
+"-o" \
+"${workspaceRoot}builds/windows/space_impact"
