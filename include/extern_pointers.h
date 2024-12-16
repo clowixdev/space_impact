136bb2b49474fba862b4246592b547421bd1e730
Nikita <clowixdev@gmail.com>
2024-12-16 19:56:00 +0300

    added new pointers

--- include/extern_pointers.h
+++ include/extern_pointers.h
@@ -11,16 +11,20 @@ extern bool changed_to_third;
 extern bool changed_to_fourth;
 extern bool player_is_dead;
 extern bool boss_is_dead;
+extern bool spawn_asteroids;
+extern bool megalovania_is_playing;
+
+extern int boss_delay;
 
 extern struct Player player;
 extern struct Boss boss;
 extern struct Heart heart;
 extern struct Menu main_menu;
 
-extern struct Bullet_list bullets;
-extern struct Bullet_list boss_bullets;
-extern struct Asteroid_list small_asteroids;
-extern struct Asteroid_list medium_asteroids;
-extern struct Asteroid_list big_asteroids;
+extern struct Bullet_list *bullets;
+extern struct Bullet_list *boss_bullets;
+extern struct Asteroid_list *small_asteroids;
+extern struct Asteroid_list *medium_asteroids;
+extern struct Asteroid_list *big_asteroids;
 
 #endif
